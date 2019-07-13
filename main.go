a4e955010a1928666beda3acf9a06a14f0e72bcc
azunymous <azu@azunymo.us>
2019-07-13 13:50:51 +0100

    Add optional resize and width only flag

--- main.go
+++ main.go
@@ -17,6 +17,18 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "imgviolence"
 	app.Usage = "Looks in a directory (deeply) in order to number, unify and resize all images"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "resize, r",
+			Usage: "resize images to 100px, 300px and 500px",
+		},
+		cli.BoolFlag{
+			Name:  "width-only, wo",
+			Usage: "resize will only resize the width of the image, keeping aspect ratio",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		if len(c.Args()) < 2 {
 			return errors.New("both an image directory and an output directory must be provided. Use -h for help")
@@ -29,7 +41,7 @@ func main() {
 		if _, err := os.Stat(saveDir); os.IsNotExist(err) {
 			_ = os.Mkdir(saveDir, os.ModeDir)
 		}
-		return filepath.Walk(dir, getImgWalker(count, saveDir))
+		return filepath.Walk(dir, getImgWalker(c, count, saveDir))
 	}
 
 	err := app.Run(os.Args)
@@ -37,7 +49,7 @@ func main() {
 		log.Fatal(err)
 	}
 }
-func getImgWalker(count int, savePath string) filepath.WalkFunc {
+func getImgWalker(c *cli.Context, count int, savePath string) filepath.WalkFunc {
 	return func(path string, f os.FileInfo, err error) error {
 		log.Println(">>>> Reading " + path)
 		if isImage(filepath.Ext(path)) {
@@ -46,7 +58,7 @@ func getImgWalker(count int, savePath string) filepath.WalkFunc {
 			if err != nil {
 				return err
 			}
-			count = processAndSaveImage(path, img, savePath, count)
+			count = processAndSaveImage(c, path, img, savePath, count)
 		}
 
 		return nil
@@ -54,7 +66,7 @@ func getImgWalker(count int, savePath string) filepath.WalkFunc {
 
 }
 
-func processAndSaveImage(path string, img image.Image, saveDir string, count int) int {
+func processAndSaveImage(c *cli.Context, path string, img image.Image, saveDir string, count int) int {
 	saveFormat := ".jpg"
 
 	baseFileName := strconv.Itoa(count)
@@ -66,18 +78,23 @@ func processAndSaveImage(path string, img image.Image, saveDir string, count int
 		saveImage(img, savePath)
 	}
 
-	resize(baseFileName, 500, saveFormat, img, saveDir)
-	resize(baseFileName, 200, saveFormat, img, saveDir)
-	resize(baseFileName, 100, saveFormat, img, saveDir)
-
+	if c.BoolT("resize") {
+		multipleResize([]int{100, 300, 500}, c.Bool("width-only"), baseFileName, saveFormat, img, saveDir)
+	}
 	return count + 1
 }
 
-func resize(baseFileName string, resizeMax int, saveFormat string, img image.Image, saveDir string) {
+func multipleResize(sizes []int, widthOnly bool, baseFileName string, saveFormat string, img image.Image, saveDir string) {
+	for _, max := range sizes {
+		resize(widthOnly, baseFileName, max, saveFormat, img, saveDir)
+	}
+}
+
+func resize(widthOnly bool, baseFileName string, resizeMax int, saveFormat string, img image.Image, saveDir string) {
 	filter := imaging.Lanczos
 	resizedFileName := baseFileName + "_" + strconv.Itoa(resizeMax) + saveFormat
 	var resizedImage = img
-	if img.Bounds().Max.X > resizeMax {
+	if img.Bounds().Max.X > resizeMax || widthOnly {
 		resizedImage = imaging.Resize(img, resizeMax, 0, filter)
 	} else if img.Bounds().Max.Y > resizeMax {
 		resizedImage = imaging.Resize(img, 0, resizeMax, filter)
